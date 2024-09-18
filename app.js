f155ab3ed161ce484eb41777de5121f4a0423dbf
maffanfarhan <muhammadaffanfarhan656@gmail.com>
2024-09-18 02:59:41 -0700

    table

--- app.js
+++ app.js
@@ -273,14 +273,14 @@ console.log("Modulus of "+num1+" and "+num2+" is "+(num1%num2));
 
 
 // ARRAYS STARTED
-answer of Q-1
+// answer of Q-1
 // Declare an empty array using JS literal notation to store           
 // student names in future.
 
 // var students_Names = [];
 // console.log(typeof(students_Names));
 
-answer of Q-2
+// answer of Q-2
 // Declare an empty array using JS object notation to store             
 // student names in future.
 
@@ -288,25 +288,25 @@ answer of Q-2
 // console.log(typeof(students_Names));
 
 
-answer of Q-3
+// answer of Q-3
 // Declare and initialize a strings array.             
 
 // var arr = ["wajahat", "minhal"]
 // console.log(arr)
 
-answer of Q-4
+// answer of Q-4
 // Declare and initialize a numbers array.             
 
 // var num = [10, 7, 4, 5]
 // console.log(num)
 
-answer of Q-5
+// answer of Q-5
 // Declare and initialize a boolean array.             
 
 // var bool = [true, false, false, true]
 // console.log(typeof(bool));
 
-answer of Q-6
+// answer of Q-6
 // Declare and initialize a mixed array.               
 
 // var mixArr = ["wajahat", 20, true]
@@ -314,7 +314,7 @@ answer of Q-6
 
 
 
-answer of Q-7
+// answer of Q-7
 // Declare and Initialize an array and store available mobile           
 // networks in Pakistan.
 
@@ -322,7 +322,7 @@ answer of Q-7
 // console.log(networks)
 
 
-answer of Q-8
+// answer of Q-8
 // Declare and Initialize an array and store available education            
 // qualifications in Pakistan (e.g. SSC, HSC, BCS, BS, BCOM, MS, 
 // M. Phil., PhD). Show the listed qualifications in your browser 
@@ -330,3 +330,195 @@ answer of Q-8
 
 
 // var education = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M.Phil", "PhD"]
+
+
+//Q1
+// var input=+prompt("Write a number: ");
+// if(input%3==0){
+//     console.log("Number is divisible by 3");
+// }
+// else{
+//     console.log("Number is not divisible by 3");
+// }
+
+//Q2
+// var input=+prompt("Give a number");
+// if(input%2==0){
+//     console.log("Its an even number");
+// }
+// else{
+//     console.log("Its an odd number");
+// }
+
+//Q3
+// var age=17;
+// if(age>18){
+//     console.log("Old Enough");
+// }
+// else{
+//     console.log("Too Young");
+// }
+
+//Q4
+// var myName=prompt("My Name");
+// var userName=prompt("User's Name");
+// if(myName==userName){
+//     console.log("Hello User!");
+// }
+// else{
+//     console.log("");
+// }
+
+
+
+
+//Q6
+// var asciiCode=+prompt("Give input")
+// if (asciiCode >= 48 && asciiCode <= 57) {
+//     console.log(asciiCode + " is a number.");
+// } else if (asciiCode >= 65 && asciiCode <= 90) {
+//     console.log(asciiCode + " is an uppercase letter.");
+// } else if (asciiCode >= 97 && asciiCode <= 122) {
+//     console.log(asciiCode + " is a lowercase letter.");
+// } else {
+//     console.log(asciiCode + " is not a number or letter.");
+// }
+
+
+
+//Q8
+// var time=+prompt("Time in 4 number digits:");
+// if(time<=2400){
+// if(time>=0 && time<1200){console.log("Good Morning")}
+// else if(time>=1200 && time<1700){
+//     console.log("Good Afternoon")
+// }
+// else if(time>=1700 && time<2100){
+//     console.log("Good Evening")
+// }
+// else{
+//     console.log("Good Night!");
+// }}
+// else{
+//     console.log("Irrelevant input!")
+// }
+
+//Q9
+// var calenderYear=+prompt("Year in 4 digit:");
+// if((calenderYear%4==0 && calenderYear%100!=0)||(calenderYear%400==0)){
+//     console.log("Its a Leap Year");
+// }
+// else {
+// console.log("Its not a Leap Year")
+// }
+
+// //Q10
+// var correctPassword="abc123";
+// var userPassword=prompt("Enter Password");
+// if(userPassword==correctPassword){
+//     console.log("Correct!");
+// }
+// else if(userPassword==""){
+//     console.log("Please enter password");
+// }
+// else if(userPassword!=correctPassword){
+//     console.log("Incorrect Password!")
+// }
+
+
+//Q11
+// var firstName="Ali";
+// if(firstName=="Fahad"){
+//     console.log("Hello Fahad!")
+// }
+// else{
+//     console.log("You are not fahad");
+// }
+
+//Q12 correction making
+// var hour=13;
+// if(hour<18){
+//     console.log("Good day")
+// }
+// else{
+//     console.log("Good Evening");
+// }
+
+
+//Q13
+// var int1=12;
+// var int2=13;
+// if(int1==int2){
+//     console.log("Both are equal")
+// }
+// else if(int1>int2){
+//     console.log(int1);
+// }
+// else{
+//     console.log(int2);
+// }
+
+//Q14
+// var input=+prompt("Give a number");
+// if(input==0){
+//     console.log("Number is zero")
+// }
+// else if(input>0){
+//     console.log("Number is positive")
+// }
+// else{
+//     console.log("Number is negative")
+// }
+
+// //Q15
+// var time=+prompt("Write to digit time from 00 to 24");
+// if(time>=6 && time<=9){
+//     console.log("Breakfast is served")
+// }
+// else if(time>=11 && time<=13){
+//     console.log("Time for Lunch")
+// }
+// else if(time>=17 && time<=20){
+//     console.log("Its dinner")
+// }
+// else{
+//     console.log("You have to wait, or go get some snacks");
+
+// }
+
+//Q16
+// var value="Hello";
+// var value1=12;
+// var value2=true;
+// var value3;
+// console.log(typeof(value));
+// console.log(typeof(value1));
+// console.log(typeof(value2));
+// console.log(typeof(value3));
+
+//Q17
+// var char="e";
+// if(char=="a"|| char=="e"||char=="i"||char=="o"||char=="u" || char=="A"||char=="E"||char=="I"||char=="O"||char=="U"){
+//    console.log("True")
+// }
+// else{
+//     console.log("False")
+// }
+
+//Q18
+// if(10!=8){
+//     console.log("True")
+// }
+
+var heading=document.querySelector("#head");
+function table(){
+    for(var i=1; i<=10; i++){
+        console.log(`${heading.value} *${i} = ${heading.value*i}`);
+    }};
+
+
+    var h1 = Document.querySelector("#table-printed");
+    
+    h1.innerhtml =   console.log(`${heading.value} *${i} = ${heading.value*i}`)
+
+
